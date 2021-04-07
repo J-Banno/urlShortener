eb146fda59ff494b03344a08f66453956166e5ed
bannholtzer jonathan <bannholtzer.j@gmail.com>
2021-04-07 12:15:11 +0200

    header

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Racourcisseur d'URL</title>
+    <link rel="stylesheet" type="text/css" href="design/default.css" media="all" />
+    <link rel="icon" type="image/png" href="./design/pictures/favico.png">
+</head>
+
+<body>
+
+
+    <!-- Présentation -->
+    <section id="hello">
+
+        <!-- Container -->
+        <div class="container">
+            <!-- Header -->
+            <header>
+                <img src="./design/pictures/logo.png" alt="logo" id="logo">
+            </header>
+            <h1>Une Url longue ? Raccourcissez-là.</h1>
+            <h2>Largement meilleur et plus court que les autres.</h2>
+            <!-- Formulaire -->
+            <form method="post" action="index.php">
+                <input type="url" name="url" placeholder="Saisissez votre Url...">
+                <input type="submit" value="Raccourcir">
+            </form>
+        </div>
+
+    </section>
+
+
+</body>
+
+</html>
